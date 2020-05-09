05bdac962babd51af550e17ea52b16c216389f94
bayramguney <bayramguney@yahoo.com>
2020-05-09 10:21:47 -0400

    conflict

--- src/github.java
+++ src/github.java
@@ -1,6 +1,7 @@
 public class github {
     public static void main(String[] args) {
         System.out.println("master is basinda");
+        System.out.println("conflict");
         System.out.println("dev 1 calisiyor");
     }
 }
